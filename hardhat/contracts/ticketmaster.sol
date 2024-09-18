602994664711de7bab8a7e304f3d06418ac801ae
Neethu-Muthu <22m014@nssce.ac.in>
2024-09-18 13:44:01 +0530

    msg

--- hardhat/contracts/ticketmaster.sol
+++ hardhat/contracts/ticketmaster.sol
@@ -9,27 +9,48 @@ contract TicketMaster {
         string venue;
         string date;
         address owner;
+        uint256 price;
+        bool isSold;
     }
 
     Ticket[] public tickets;
     mapping(uint256 => address) public ticketOwners;
 
-    event TicketPurchased(uint256 id, address buyer);
+    event TicketPurchased(uint256 indexed id, address buyer);
+    event TicketCreated(uint256 indexed id, string name, string venue, string date, uint256 price);
 
-    function createTicket(string memory _name, string memory _venue, string memory _date) public {
+    function createTicket(string memory _name, string memory _venue, string memory _date, uint256 _price) public {
         uint256 ticketId = tickets.length;
-        tickets.push(Ticket(ticketId, _name, _venue, _date, address(0)));
+        tickets.push(Ticket(ticketId, _name, _venue, _date, address(0), _price, false));
+        emit TicketCreated(ticketId, _name, _venue, _date, _price);
     }
 
     function buyTicket(uint256 _ticketId) public payable {
         require(_ticketId < tickets.length, "Ticket does not exist.");
-        require(ticketOwners[_ticketId] == address(0), "Ticket already purchased.");
+        Ticket storage ticket = tickets[_ticketId];
+        require(!ticket.isSold, "Ticket already purchased.");
+        require(msg.value >= ticket.price, "Insufficient payment.");
+
+        // Mark the ticket as sold
+        ticket.owner = msg.sender;
+        ticket.isSold = true;
         ticketOwners[_ticketId] = msg.sender;
+
+        // Transfer the payment to the contract owner
+        payable(address(this)).transfer(msg.value);
+        
         emit TicketPurchased(_ticketId, msg.sender);
     }
 
     function getTicket(uint256 _ticketId) public view returns (Ticket memory) {
         require(_ticketId < tickets.length, "Ticket does not exist.");
         return tickets[_ticketId];
+    }
+
+    // Optional: Add a function to withdraw collected funds (onlyOwner pattern can be used)
+    function withdrawFunds() public {
+        // Only allow the contract owner to withdraw
+        require(msg.sender == address(this));
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
